94fac1d40c8105f6e5b07e60612d82e26032fa5f
porras92 <t.porras07@gmail.com>
2016-04-19 08:56:24 +0200

    prueba1

--- src/test/MainaderiaTest.java
+++ src/test/MainaderiaTest.java
@@ -19,7 +19,7 @@ public class MainaderiaTest {
                 new Criatura("PERE",1, Criatura.NEN),
                 new Criatura("NEUS",0, Criatura.NENA),
                 new Criatura("ONA",1, Criatura.NENA),
-                new Criatura("DÍDAC",0, Criatura.NEN),
+                new Criatura("DÃ­DAC",0, Criatura.NEN),
                 new Criatura("MARIONA",1, Criatura.NENA),
                 new Criatura("EVA",3, Criatura.NENA),
                 new Criatura("FIONA",2, Criatura.NENA),
